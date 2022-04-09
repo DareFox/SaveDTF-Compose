8477d22f69a9844254abdb13f350d47e004d95de
DareFox <easymalink@gmail.com>
2022-04-10 00:57:03 +0300

    Add kmttAPI and other dependecies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,29 @@ version = "1.0"
 repositories {
     google()
     mavenCentral()
+    maven("https://jitpack.io")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
+
+val ktor_version = "1.6.8"
 
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation("br.com.devsrsouza.compose.icons.jetbrains:feather:1.0.0")
+
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+
+    implementation("com.github.DareFox:kmttAPI:0.2.0")
+
+    implementation("org.jsoup:jsoup:1.14.3")
+
+    implementation("io.github.resilience4j:resilience4j-ratelimiter:1.7.1")
+    implementation("io.github.resilience4j:resilience4j-kotlin:1.7.1")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 compose.desktop {
---
+++ src/main/kotlin/logic/ktor/Client.kt
@@ -0,0 +1,32 @@
+package logic.ktor
+
+import io.github.resilience4j.kotlin.ratelimiter.executeSuspendFunction
+import io.github.resilience4j.ratelimiter.RateLimiter
+import io.github.resilience4j.ratelimiter.RateLimiterConfig
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry
+import io.ktor.client.*
+import io.ktor.client.request.*
+import java.time.Duration
+import java.util.*
+
+object Client {
+    private val rateLimitID = UUID.randomUUID().toString()
+    private val rateLimiterRegistry = RateLimiterRegistry.of(RateLimiterConfig
+        .custom()
+        .limitRefreshPeriod(Duration.ofSeconds(1))
+        .limitForPeriod(3)
+        .timeoutDuration(Duration.ofHours(300))
+        .build())
+
+    val rateLimiter: RateLimiter = rateLimiterRegistry.rateLimiter(rateLimitID)
+    val httpClient: HttpClient = HttpClient()
+}
+
+/**
+ * Send request with rate limit
+ */
+suspend inline fun <reified T> Client.rateRequest(crossinline block: HttpRequestBuilder.() -> Unit): T {
+    return rateLimiter.executeSuspendFunction {
+        httpClient.request(block)
+    }
+}
