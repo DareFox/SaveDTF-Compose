08bca1556fd6920e841235a95a59a314133dffff
DareFox <easymalink@gmail.com>
2022-04-12 01:56:56 +0300

    Add logs to EntryDownloader

--- src/main/kotlin/logic/downloaders/EntryDownloader.kt
+++ src/main/kotlin/logic/downloaders/EntryDownloader.kt
@@ -3,6 +3,7 @@ package logic.downloaders
 import io.ktor.client.features.*
 import kmtt.models.entry.Entry
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.yield
@@ -10,11 +11,15 @@ import logic.document.processors.downloadDocument
 import logic.document.processors.reformat
 import logic.document.processors.removeStyles
 import logic.downloaders.exceptions.NoContentDownloadedException
+import mu.KotlinLogging
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import util.getTempCacheFolder
 import java.io.File
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.math.log
+
+private val logger = KotlinLogging.logger { }
 
 private class EntryDownloader(override val entry: Entry) : IEntryDownloader {
     private var document: Document
@@ -59,6 +64,7 @@ private class EntryDownloader(override val entry: Entry) : IEntryDownloader {
 
             return _isDownloaded.value
         } catch (cancel: CancellationException) {
+            logger.info { "Download of entry (id: ${entry.id}) was cancelled" }
              files = null
             _isDownloaded.value = false
             document = Jsoup.parse(entry.entryContent!!.html!!) // Force unwrap because class and variable are immutable (we check null in init already)
@@ -70,29 +76,33 @@ private class EntryDownloader(override val entry: Entry) : IEntryDownloader {
         if (files == null) {
             throw NoContentDownloadedException("No files were downloaded. Please call download() before save()")
         } else {
-            // TODO add synchronized version of save method
-            val binaryFiles = files!!
+            // TODO add synchronized version of save method (???)
+            val binaryFiles = files!!.toMutableMap()
             val writtenFiles = mutableListOf<File>()
 
             try {
+                yield()
+                binaryFiles["index.html"] = document.toString().toByteArray() // Include binary HTML too
+
                 binaryFiles.forEach { (path, binary) ->
                     yield()
+
                     val toWrite = file.resolve(path)
                     toWrite.parentFile.mkdirs() // Create dirs to file, but don't make binary file as new dir too
+
+                    logger.info { "Writing ${binary.size}B to ${toWrite.absolutePath}" }
                     toWrite.writeBytes(binary)
                     writtenFiles += toWrite
                 }
 
-                yield()
-                val index = file.resolve("index.html").also { writtenFiles += it }
-                index.writeText(document.toString())
-                yield()
-
             } catch (cancel: CancellationException) {
+                logger.info { "Save operation of entry (id: ${entry.id}) was cancelled. Deleting all created files" }
                 // Delete written files on cancellation
                 writtenFiles.forEach {
+                    logger.info { "Deleting ${it.absolutePath}..." }
                     it.delete()
                 }
+                logger.info { "All ${writtenFiles.size} files were deleted" }
                 throw cancel
             }
         }
