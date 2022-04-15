7f23e359bca922b39943f911f55cd64b632847a6
DareFox <easymalink@gmail.com>
2022-04-15 19:32:59 +0300

    SettingsViewModels: Change token from String to Map

--- src/main/kotlin/ui/viewmodel/SettingsViewModel.kt
+++ src/main/kotlin/ui/viewmodel/SettingsViewModel.kt
@@ -1,24 +1,35 @@
 package ui.viewmodel
 
+import kmtt.models.enums.Website
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import java.io.File
 import java.util.prefs.Preferences
 
 object SettingsViewModel {
     private val preferences = Preferences.userRoot().node("savedtf-prefs")
 
-    private val _token = MutableStateFlow<String?>(getPrefToken())
-    private fun getPrefToken() = preferences.get("tkn", null)
-    val token: StateFlow<String?> = _token
-
     private val _folderToSave = MutableStateFlow<String>(getPrefFolder())
     private fun getPrefFolder() = preferences.get("save_folder", File("./saved").canonicalPath)
     val folderToSave: StateFlow<String?> = _folderToSave
 
-    fun setToken(token: String) {
-        preferences.put("tkn", token)
-        _token.value = getPrefToken()
+    private fun getToken(website: Website) = preferences.node("tkn").get(website.name, null)
+    private val _tokens = MutableStateFlow(mapOf<Website, String?>(
+        Website.DTF to getToken(Website.DTF),
+        Website.VC to getToken(Website.VC),
+        Website.TJ to getToken(Website.TJ)
+    ))
+    val tokens: StateFlow<Map<Website, String?>> = _tokens
+
+    fun setToken(token: String, website: Website) {
+        preferences.node("tkn").put(website.name, token)
+
+        _tokens.update {
+            val map = it.toMutableMap()
+            map[website] = getToken(website)
+            map
+        }
     }
 
     fun setFolderToSave(folder: String) {
