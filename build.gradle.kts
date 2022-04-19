2d871b948c9b3938e5576b01f98b54441c8329b5
DareFox <easymalink@gmail.com>
2022-04-20 01:05:32 +0300

    Gradle & App: Add logger to file
    It will gap log to 1mb (1 file) and archive it. Archive is limited too, 10MB is limit for it

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,8 +48,11 @@ dependencies {
     implementation("io.github.resilience4j:resilience4j-kotlin:1.7.1")
 
     // Logger
-    implementation("io.github.microutils:kotlin-logging-jvm:2.1.20")
-    implementation("org.slf4j:slf4j-simple:1.7.29")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("ch.qos.logback:logback-core:1.2.11")
+
 
 }
 
