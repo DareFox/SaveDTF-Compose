82f1904c4aef4d4dca7850982bdb253c0956b7b4
DareFox <easymalink@gmail.com>
2022-04-19 18:27:22 +0300

    QueueCreatorMenu: Add buttons to download/remove/update all

--- src/main/kotlin/ui/menus/QueueCreatorMenu.kt
+++ src/main/kotlin/ui/menus/QueueCreatorMenu.kt
@@ -2,7 +2,6 @@ package ui.menus
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
@@ -12,6 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Download
+import compose.icons.feathericons.RefreshCcw
+import compose.icons.feathericons.Trash
+import kotlinx.coroutines.launch
 import ui.SaveDtfTheme
 import ui.composables.FancyButton
 import ui.composables.FancyInputField
@@ -28,16 +30,18 @@ fun QueueCreatorMenuPreview() {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun QueueCreatorMenu() {
+    val viewmodel = QueueViewModel
+    val queue by viewmodel.queue.collectAsState()
+
     Surface(Modifier.fillMaxWidth()) {
         Column(modifier = Modifier.padding(10.dp, 0.dp)) {
-            val viewmodel = QueueViewModel
 
             var input by rememberSaveable { mutableStateOf("") }
             var enable by rememberSaveable { mutableStateOf(false) }
 
 
             val supported = viewmodel.canCreateQueueElement(input)
-            val errorMessage = if(input.isNotEmpty() && !supported) "Неверный URL" else null
+            val errorMessage = if (input.isNotEmpty() && !supported) "Неверный URL" else null
 
             enable = input.isNotEmpty() && supported
 
@@ -50,17 +54,67 @@ fun QueueCreatorMenu() {
                 },
                 input = input,
                 placeholderInput = "Вставь ссылку сюда",
-                placeholderButton = "Добавить в очередь",
                 enabled = enable,
                 isError = input.isNotEmpty() && !supported,
                 errorMessage = errorMessage
-             )
+            )
             Spacer(modifier = Modifier.fillMaxWidth().height(10.dp))
-            FancyButton(true, onClick = {
-                viewmodel.createAndAddQueueElement(input)
-            }, "Добавить в очередь")
+
+            val isQueueNotEmpty = queue.isNotEmpty()
+            val scope = rememberCoroutineScope()
+
+            val buttons = mutableListOf<@Composable () -> Unit>(
+                {
+                    // Download all button
+                    FancyButton(isQueueNotEmpty, onClick = {
+                        queue.forEach {
+                            scope.launch {
+                                it.save()
+                            }
+                        }
+                    }) {
+                        Icon(FeatherIcons.Download, null)
+                    }
+                },
+                {
+                    // Remove all from queue
+                    FancyButton(isQueueNotEmpty, onClick = {
+                        viewmodel.clear()
+                    }) {
+                        Icon(FeatherIcons.Trash, null)
+                    }
+                },
+                {
+                    // Update all elements
+                    FancyButton(isQueueNotEmpty, onClick = {
+                        viewmodel.queue.value.forEach {
+                            scope.launch {
+                                it.initialize()
+                            }
+                        }
+                    }) {
+                        Icon(FeatherIcons.RefreshCcw, null)
+                    }
+                }
+                )
+
+            Row {
+                Surface(modifier = Modifier.weight(1f)) {
+                    FancyButton(true, onClick = {
+                        viewmodel.createAndAddQueueElement(input)
+                    }, "Добавить в очередь")
+                }
+                buttons.forEach {
+                    // add buttons
+                    Surface(modifier = Modifier.padding(start = 10.dp).width(50.dp)) {
+                        it()
+                    }
+                }
+            }
+
 
             QueueList()
         }
+
     }
 }
