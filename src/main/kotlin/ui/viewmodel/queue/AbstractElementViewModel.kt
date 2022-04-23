d1f1245e42ae7250dd2c43d7bf2d97730a0ddb9b
DareFox <easymalink@gmail.com>
2022-04-23 21:53:18 +0300

    ElementVM: Create an abstract class for standard field implementations

---
+++ src/main/kotlin/ui/viewmodel/queue/AbstractElementViewModel.kt
@@ -0,0 +1,41 @@
+package ui.viewmodel.queue
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import ui.viewmodel.SettingsViewModel
+import ui.viewmodel.queue.IQueueElementViewModel.QueueElementStatus
+
+abstract class AbstractElementViewModel: IQueueElementViewModel {
+    protected val _status = MutableStateFlow(QueueElementStatus.INITIALIZING)
+    override val status: StateFlow<QueueElementStatus> = _status
+
+    protected val _lastErrorMessage = MutableStateFlow<String?>(null)
+    override val lastErrorMessage: StateFlow<String?> = _lastErrorMessage
+
+    protected val _progress = MutableStateFlow<String?>(null)
+    override val progress: StateFlow<String?> = _progress
+
+    protected var customPath: String? = null
+    override val pathToSave: String?
+        get() = customPath ?: SettingsViewModel.folderToSave.value
+
+    override fun setPathToSave(folder: String) {
+        customPath = folder
+    }
+
+    protected val _selected = MutableStateFlow(false)
+    override val selected: StateFlow<Boolean> = _selected
+
+    override fun select() {
+        _selected.value = true
+    }
+
+    override fun unselect() {
+        _selected.value = false
+    }
+
+    protected fun error(message: String) {
+        _lastErrorMessage.value = message
+        _status.value = QueueElementStatus.ERROR
+    }
+}
