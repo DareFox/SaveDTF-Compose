9592276e383162111b15c0060923f7ef45928e62
DareFox <easymalink@gmail.com>
2022-04-24 00:14:02 +0300

    ValidateFilename: change validation method

--- src/main/kotlin/util/ValidateFilename.kt
+++ src/main/kotlin/util/ValidateFilename.kt
@@ -2,19 +2,31 @@ package util
 
 import io.ktor.utils.io.errors.*
 import java.io.File
+import java.nio.file.InvalidPathException
+import java.nio.file.Paths
 
-fun convertToValidName(name: String): String {
+fun convertToValidName(name: String, default: String = "null"): String {
     val file = File(name)
-    val isValid = try {
-        file.canonicalPath
-        true
-    } catch (_: IOException) {
-        false
-    }
-
-    return if (isValid) {
+    val newName = if (isValid(file)) {
         name
     } else {
         SharedRegex.filenameValidationRegex.replace(name, "")
     }
+
+    // Check if new name is valid, if not, return default
+    return if(isValid(File(newName))) {
+        newName
+    } else {
+        default
+    }
+}
+
+private fun isValid(file: File): Boolean {
+    return try {
+        Paths.get(file.path)
+        true
+    } catch (_: InvalidPathException) {
+        false
+    }
+
 }
