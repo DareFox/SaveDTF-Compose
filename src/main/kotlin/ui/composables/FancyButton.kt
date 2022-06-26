7e83b2999026590f28ddc2499d9dbd446f24db3f
DareFox <easymalink@gmail.com>
2022-06-26 17:24:45 +0300

    FancyButton: Add onDisabledClick() listener

--- src/main/kotlin/ui/composables/FancyButton.kt
+++ src/main/kotlin/ui/composables/FancyButton.kt
@@ -1,8 +1,7 @@
 package ui.composables
 
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -17,6 +16,7 @@ fun FancyButton(
     enabled: Boolean,
     onClick: () -> Unit,
     placeholderButton: String,
+    onDisabledClick: () -> Unit = {},
     buttonColors: ButtonColors = ButtonDefaults.buttonColors(),
 ) {
     FancyButton(
@@ -29,7 +29,8 @@ fun FancyButton(
                 style = MaterialTheme.typography.subtitle2,
                 fontWeight = FontWeight.Bold
             )
-        }
+        },
+        onDisabledClick = onDisabledClick
     )
 }
 
@@ -38,13 +39,30 @@ fun FancyButton(
     enabled: Boolean,
     onClick: () -> Unit,
     buttonColors: ButtonColors = ButtonDefaults.buttonColors(),
+    onDisabledClick: () -> Unit = {},
     placeholderButton: @Composable RowScope.() -> Unit,
 ) {
-    Button(
-        enabled = enabled,
-        onClick = onClick,
-        content = placeholderButton,
-        modifier = Modifier.fillMaxWidth().defaultMinSize(Dp.Unspecified, 50.dp).clip(shape = RoundedCornerShape(25)),
-        colors = buttonColors
-    )
+    /* Use IntrinsicSize.Min to clip box inside surface */
+    Surface(Modifier.height(IntrinsicSize.Min).width(IntrinsicSize.Min)) {
+        Button(
+            enabled = enabled,
+            onClick = onClick,
+            content = placeholderButton,
+            modifier = Modifier.fillMaxWidth().defaultMinSize(Dp.Unspecified, 50.dp)
+                .clip(shape = RoundedCornerShape(25)),
+            colors = buttonColors,
+        )
+        if (!enabled) {
+            /* When button is disabled, hijack clicks from disabled button
+               to invisible box (which have exact shape as button)
+
+               Why don't use box/surface with button in it?
+               Because even if button is disabled, it will consume clicks
+               So we need to "hijack" them from button by placing transparent box on top of it
+            */
+            Box(Modifier.fillMaxHeight().fillMaxWidth().clip(shape = RoundedCornerShape(25)).clickable {
+                onDisabledClick()
+            })
+        }
+    }
 }
