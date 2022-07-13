6d361072654c855ce32f7e02e3d3f3209bfe3ff8
darefox <easymalink@gmail.com>
2022-07-13 20:08:16 +0300

    UI-Theme: Add successVariant color

--- src/main/kotlin/ui/theme/CustomPallet.kt
+++ src/main/kotlin/ui/theme/CustomPallet.kt
@@ -7,6 +7,8 @@ object CustomPallet {
     val success = Color.Green
     val onSuccess = Color.Black
 
+    val successVariant = Color.Green.copy(0.7f)
+
     val error = Color.Red
     val onError = Color.Black
 }
