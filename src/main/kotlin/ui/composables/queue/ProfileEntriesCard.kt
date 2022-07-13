5ded544dd87af4e2ad158426c2b99792b6249192
darefox <easymalink@gmail.com>
2022-07-14 00:25:16 +0300

    UI-ProfileCard: Use text from l10n

--- src/main/kotlin/ui/composables/queue/ProfileEntriesCard.kt
+++ src/main/kotlin/ui/composables/queue/ProfileEntriesCard.kt
@@ -3,22 +3,30 @@ package ui.composables.queue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import ui.i18n.Lang
 import ui.viewmodel.queue.IBookmarksElementViewModel
 import ui.viewmodel.queue.IProfileElementViewModel
 import ui.viewmodel.queue.IQueueElementViewModel
 
 @Composable
 fun ProfileCard(viewModel: IProfileElementViewModel, actionBar: List<ActionBarElement> = listOf()) {
+    val lang by Lang.collectAsState()
     val status by viewModel.status.collectAsState()
     val error by viewModel.lastErrorMessage.collectAsState()
     val author by viewModel.user.collectAsState()
-    val title = "All profile entries"
+    val immutableAuthor = author
+
+    val title = if (immutableAuthor != null) {
+        lang.profileElementVmCardTitleWithName.format(immutableAuthor.name)
+    } else {
+        lang.profileElementVmCardTitleWithID.format(viewModel.id)
+    }
 
     SimpleCard(
         viewModel = viewModel,
         actionBar = actionBar,
         title = title,
-        author = author?.name ?: "null",
+        author = viewModel.site.name,
         status = status,
         error = if (status == IQueueElementViewModel.QueueElementStatus.ERROR) error else null,
         website = viewModel.site
