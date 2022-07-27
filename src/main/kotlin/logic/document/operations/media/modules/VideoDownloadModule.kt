8b4e23900515259e571419b60d67077231073f53
darefox <easymalink@gmail.com>
2022-07-27 04:14:46 +0300

    VideoDownloadModule: Add support for gallery video

--- src/main/kotlin/logic/document/operations/media/modules/VideoDownloadModule.kt
+++ src/main/kotlin/logic/document/operations/media/modules/VideoDownloadModule.kt
@@ -11,6 +11,10 @@ object VideoDownloadModule: IDownloadModule {
     override val onErrorMedia: BinaryMedia? = Resources.videoLoadFail
 
     override fun filter(document: Document): List<Pair<Element, String>> {
+        return getGalleryVideoContainers(document) + getRegularVideoContainers(document)
+    }
+
+    private fun getRegularVideoContainers(document: Document): List<Pair<Element, String>> {
         return document.getElementsByClass("andropov_video").filter {
             it.attr("data-video-mp4").isNotEmpty()
         }.map {
@@ -18,18 +22,53 @@ object VideoDownloadModule: IDownloadModule {
         }
     }
 
+    private fun getGalleryVideoContainers(document: Document): List<Pair<Element, String>> {
+        // Convert each gallery to list of pairs
+        return document.getElementsByClass("gall").mapNotNull {
+            // And to do this, convert their children to list of pairs
+            it.children().mapNotNull childConvert@ { div ->
+                // If type or url is empty, then skip it by returning null
+                val type = div.attr("media-type").ifEmpty { return@childConvert null  }
+                val url = div.attr("media-url").ifEmpty { return@childConvert null  }
+
+                if (type != "video") return@childConvert null
+
+                addVideoPreviewElement(div) to url
+            }
+
+
+        }.flatten() // Then flat all list of galleries to have one big list
+    }
+
+    private fun addVideoPreviewElement(div: Element): Element {
+        val videoPreviewElement = Element("video").also {
+            it.attr("autoplay", "")
+            it.attr("muted", "")
+            it.attr("loop", "")
+            it.addClass("gall-vid-preview")
+        }
+
+        div.prependChild(videoPreviewElement)
+
+        return videoPreviewElement
+    }
+
     override fun transform(element: Element, relativePath: String) {
-        val sourceElement = Element("source").attr("src", relativePath)
+        if (element.hasClass("gall-vid-preview")) {
+            element.attr("src", relativePath)
+        } else {
+            val sourceElement = Element("source").attr("src", relativePath)
 
-        val base = Element("video")
-            .attr("controls", "")
-            .attr("style", "height: 100%; width: 100%; object-fit: contain")
-            .prependChild(sourceElement)
+            val base = Element("video")
+                .attr("controls", "")
+                .attr("style", "height: 100%; width: 100%; object-fit: contain")
+                .prependChild(sourceElement)
 
 
-        element
-            .recreateWithoutNodes()
-            .prependChild(base)
+            element
+                .recreateWithoutNodes()
+                .prependChild(base)
+        }
     }
 
     override val downloadingContentType: String
