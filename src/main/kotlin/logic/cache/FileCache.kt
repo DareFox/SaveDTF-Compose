9dcdb3f02c9fcbb9f2dc3c948b14ed0af4e70789
darefox <easymalink@gmail.com>
2022-08-07 23:40:45 +0300

    FileCache: Support cleaning temp folder
    When remaning size is less than 2GB, it will remove other files to make room for given file

--- src/main/kotlin/logic/cache/FileCache.kt
+++ src/main/kotlin/logic/cache/FileCache.kt
@@ -1,12 +1,14 @@
 package logic.cache
 
 import mu.KotlinLogging
+import util.cache.postfix
+import util.cache.prefix
 import util.filesystem.convertToValidName
 import util.filesystem.getTempCacheFolder
 import util.filesystem.recursiveFileList
-import util.filesystem.recursiveForEach
 import java.io.File
 import java.io.IOException
+import java.text.DecimalFormat
 
 /**
  * Represents cache on hard drive level.
@@ -18,13 +20,25 @@ internal class FileCache(val subdirName: String? = null) : BinaryCache {
         get() = getTempCacheFolder(subdirName)
 
     override fun setValue(key: String, value: ByteArray) {
+        makeRoomForFile(value)
         if (tempFolder.freeSpace < value.size) {
             logger.warn {
                 "Can't save to file cache because disk ran out of space"
             }
         } else {
-            tempFolder.resolve(convertToValidName(key)).writeBytes(value)
+            val file = tempFolder.resolve(convertToValidName(key))
 
+            try {
+                file.writeBytes(value)
+            } catch (ex: Exception) {
+                logger.error { "Caught error when trying to save cache to file:\n" +
+                        "[${ex.javaClass.simpleName}]  ${ex.message}\n" +
+                        ex.stackTraceToString()
+                }
+                try {
+                    file.delete()
+                } catch (_: Exception) {logger.error { "Can't clean file on error" }}
+            }
         }
     }
 
@@ -44,28 +58,91 @@ internal class FileCache(val subdirName: String? = null) : BinaryCache {
         val isEnoughSpaceForData = tempFolder.freeSpace - data.size >= freeSpaceReservationInBytes
 
         if (isEnoughSpaceForData) return
+        logger.info { "Not enough space for data. Trying to make room for file" }
 
         val excessSize = (freeSpaceReservationInBytes - tempFolder.freeSpace) + data.size
         var releasedSpace = 0L
 
+        logger.info { "Excess size = (${sizeToString(excessSize)})" }
+        logger.info { "File size = (${sizeToString(data.size.toLong())})" }
+        logger.info { "Temp folder space = (${sizeToString(tempFolder.freeSpace)})" }
         for (file in tempFolder.recursiveFileList().sortedBy(File::lastModified)) {
             if (releasedSpace >= excessSize) {
+                logger.info { "Released space (${sizeToString(releasedSpace)}) is bigger than excess size. Abort cleaning" }
                 break;
             }
 
             try {
-                val expectedReleasedSpaceBytes = file.length()
+                val filename = file.name
+                val isMetadataFile = filename.startsWith(prefix) && filename.endsWith(postfix)
+                val fileList = mutableListOf(file)
 
-                if (file.delete()) {
-                    releasedSpace += expectedReleasedSpaceBytes
+                // Delete cached value with metadata too and vise versa
+                if (isMetadataFile) {
+                    val cachedFilename = filename.removePrefix(prefix).removeSuffix(postfix)
+                    val cachedFile = file.resolveSibling(cachedFilename)
+
+                    if (cachedFile.isFile) {
+                        logger.debug { "Found cached pair value to delete" }
+                        fileList += cachedFile
+                    } else {
+                        logger.debug { "Can't find cached pair value to delete" }
+                    }
+                } else {
+                    val metadataFile = file.resolveSibling(prefix + filename + postfix)
+
+                    if (metadataFile.isFile) {
+                        logger.debug { "Found metadata pair value to delete" }
+                        fileList += metadataFile
+                    } else {
+                        logger.debug { "Can't find metadata pair value to delete" }
+                    }
+                }
+
+                fileList.forEach { deleteFile ->
+                    val deletedFilename = deleteFile.name
+                    val fileSizeInBytes = deleteFile.length()
+                    if (deleteFile.delete()) {
+                        logger.info {
+                            "Deleted $deletedFilename file " +
+                                    "(${sizeToString(fileSizeInBytes)})"
+                        }
+                        releasedSpace += fileSizeInBytes
+                    }
                 }
             } catch (ex: IOException) {
                 logger.debug {
-                    "Error during file deletion: [${ex.javaClass.simpleName}]  ${ex.message}\n" +
+                    "Caught error during file deletion:\n" +
+                            "[${ex.javaClass.simpleName}]  ${ex.message}\n" +
                             ex.stackTraceToString()
                 }
             }
         }
+    }
+
+     private fun sizeToString(size: Long): String {
+        val kilobyte = 1024
+        val megabyte = kilobyte * 1024
+
+        var result = 0f
+        var type = ""
+
+        when {
+            size >= megabyte -> {
+                result = size / megabyte.toFloat()
+                type = "MB"
+            }
+            size >= kilobyte -> {
+                result = size / kilobyte.toFloat()
+                type = "KB"
+            }
+            else -> {
+                return "$size B"
+            }
+        }
+
+        val decimalFormat = DecimalFormat("#.##")
+        return "${decimalFormat.format(result)} $type"
     }
 
     override fun remove(key: String): Boolean {
@@ -81,3 +158,4 @@ internal class FileCache(val subdirName: String? = null) : BinaryCache {
         logger.info { "Clearing all file cache" }
         return tempFolder.deleteRecursively()
     }
+}
