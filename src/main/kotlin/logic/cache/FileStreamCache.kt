a083ff863e84beea472d264ff88be179b7fc37b0
darefox <easymalink@gmail.com>
2022-08-14 15:33:07 +0300

    FileStreamCache: Add auto-cleaning support
    Just like in previously FileCache (BinaryCache)
    
    Max size of cache: 4GB
    Min free space of cache folder: 2GB

--- src/main/kotlin/logic/cache/FileStreamCache.kt
+++ src/main/kotlin/logic/cache/FileStreamCache.kt
@@ -1,11 +1,19 @@
 
 package logic.cache
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import logic.io.WaitingMutex
 import logic.io.asCounter
 import logic.io.asMultiStream
 import mu.KotlinLogging
 import util.filesystem.getTempCacheFolder
+import util.filesystem.recursiveFileList
+import util.filesystem.sizeToString
 import java.io.File
+import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.*
@@ -15,6 +23,9 @@ object FileStreamCache: StreamCache {
     private val tempFolder = getTempCacheFolder()
     private val fileMap = FileMap.createMap(tempFolder)
     private val logger = KotlinLogging.logger {  }
+    private val waitingMutex = WaitingMutex(CoroutineScope(Dispatchers.IO + SupervisorJob()))
+    private val freeSpaceReservationInMb: Long = 2048
+    private val maxSizeOfFileCacheInMb: Long = 4096
 
     override fun setValue(key: String, value: InputStream) {
         setValue(key, value, emptyList())
@@ -34,9 +45,10 @@ object FileStreamCache: StreamCache {
 
         value.copyTo((redirectTo + counterFileStream).asMultiStream())
         logger.debug {
-            "Saved key $key to file ${file.absolutePath}. Size of file: $"
+            "Saved key $key to file ${file.absolutePath}. Size of file: ${sizeToString(counterFileStream.counter)}"
         }
         fileMap.addKey(key, file)
+        makeRoomForFile()
     }
 
     override fun setValue(key: String, value: InputStream, redirectTo: OutputStream) {
@@ -60,6 +72,130 @@ object FileStreamCache: StreamCache {
     override fun clearAll(): Boolean {
         return tempFolder.deleteRecursively().also {
             if (it) fileMap.clearAll()
+        }
+    }
+
+    private fun makeRoomForFile() {
+        waitingMutex.tryToRun {
+            val freeSpaceReservationInBytes = freeSpaceReservationInMb * 1048576L
+            val maxSizeOfFileCacheInBytes = maxSizeOfFileCacheInMb * 1048576L
+
+            val files = tempFolder.recursiveFileList()
+
+            // File.length() on directory returns unspecified value
+            // So we need to sum files length
+            val folderLength: Long = files.sumOf { it.length() }
+
+            val isNotEnoughSpace = tempFolder.freeSpace  < freeSpaceReservationInBytes
+            val fileCacheMaxedOut = folderLength >= maxSizeOfFileCacheInBytes
+
+            val excessReservedSpace = freeSpaceReservationInBytes - tempFolder.freeSpace
+            val excessCachedMaxedOutSpace = folderLength - maxSizeOfFileCacheInBytes
+
+            if (isNotEnoughSpace) {
+                logger.info { "Not enough space for data. Trying to make room for file" }
+            }
+
+            if (fileCacheMaxedOut) {
+                logger.info { "File cached maxed out. Trying to make room for file" }
+            }
+
+            if (!isNotEnoughSpace && !fileCacheMaxedOut) {
+                delay(1500L)
+                return@tryToRun
+            }
+
+            // Math.max(), but Kotlin way
+            val toRelease = excessReservedSpace.coerceAtLeast(excessCachedMaxedOutSpace)
+
+            logger.info { "Excess size = (${sizeToString(toRelease)})" }
+            logger.info { "Temp folder free space = (${sizeToString(tempFolder.freeSpace)})" }
+            logger.info { "Temp folder size = (${sizeToString(folderLength)})" }
+
+            freeBytes(toRelease)
+            delay(1500L)
+        }
+    }
+
+    private fun freeBytes(bytes: Long) {
+        val nonExistentFiles = mutableMapOf<File, MutableList<String>>()
+        var releasedSpace = 0L
+
+        val canonicalFileToKeys = fileMap.map.mapNotNull {
+            try {
+                it.value.canonicalFile to it.key
+            } catch (e: Exception) {
+                nonExistentFiles[it.value] = nonExistentFiles.getOrDefault(it.value, mutableListOf()).apply {
+                    add(it.key)
+                }
+                null
+            }
+        }.groupBy({it.first}) {
+            it.second
+        }
+
+        nonExistentFiles.entries.forEach {
+            val size = it.key.length()
+            if (tryToDelete(it.key)) {
+                logger.info {
+                    "Deleted ${it.key.absolutePath} file (${sizeToString(size)})"
+                }
+                releasedSpace += size
+            }
+            it.value.forEach { key ->
+                fileMap.removeKey(key)
+            }
+
+            if (releasedSpace >= bytes) {
+                logger.info {
+                    "Released space (${sizeToString(releasedSpace)}) is bigger than given (${sizeToString(bytes)}) size. Abort cleaning"
+                }
+                return;
+            }
+        }
+
+        val files = tempFolder.recursiveFileList()
+
+        for (file in files.sortedBy(File::lastModified)) {
+            if (file.extension == "json") continue
+
+            val deletedFilename = file.name
+            val canonical = try { file.canonicalFile } catch (e: Exception) { null }
+            val fileSizeInBytes = file.length()
+
+            if (tryToDelete(file)) {
+                logger.info {
+                    "Deleted $deletedFilename file " +
+                            "(${sizeToString(fileSizeInBytes)})"
+                }
+                releasedSpace += fileSizeInBytes
+
+                if (canonical != null) {
+                    canonicalFileToKeys[canonical]?.forEach {
+                        fileMap.removeKey(it)
+                    }
+                }
+            }
+
+            if (releasedSpace >= bytes) {
+                logger.info {
+                    "Released space (${sizeToString(releasedSpace)}) is bigger than given (${sizeToString(bytes)}) size. Abort cleaning"
+                }
+                return;
+            }
+        }
+    }
+
+    private fun tryToDelete(file: File): Boolean {
+        return try {
+            file.delete()
+        } catch (ex: IOException) {
+            logger.warn {
+                "Caught error during file deletion:\n" +
+                        "[${ex.javaClass.simpleName}]  ${ex.message}\n" +
+                        ex.stackTraceToString()
+            }
+            false
         }
     }
 }
