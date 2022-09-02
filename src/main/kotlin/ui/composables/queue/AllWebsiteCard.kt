fda4d703ac5faa830ac8182da35d617ab6ecf5f5
darefox <easymalink@gmail.com>
2022-09-02 22:49:33 +0300

    ALlEntriesCard: use l10n

--- src/main/kotlin/ui/composables/queue/AllWebsiteCard.kt
+++ src/main/kotlin/ui/composables/queue/AllWebsiteCard.kt
@@ -3,6 +3,7 @@ package ui.composables.queue
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import kotlinx.coroutines.flow.compose
 import ui.i18n.Lang
 import viewmodel.queue.AllEntriesViewModel
 import viewmodel.queue.IProfileElementViewModel
@@ -12,11 +13,12 @@ import viewmodel.queue.IQueueElementViewModel
 fun AllWebsiteCard(viewModel: AllEntriesViewModel, actionBar: List<ActionBarElement> = listOf()) {
     val status by viewModel.status.collectAsState()
     val error by viewModel.lastErrorMessage.collectAsState()
+    val lang by Lang.collectAsState()
 
     SimpleCard(
         viewModel = viewModel,
         actionBar = actionBar,
-        title = "All entries",
+        title = lang.allEntriesVmCardTitle,
         author = viewModel.site.name,
         status = status,
         error = if (status == IQueueElementViewModel.QueueElementStatus.ERROR) error else null,
