1f14d19daf5bd32b7406d6105ec9535dfb750f58
darefox <easymalink@gmail.com>
2022-09-08 19:58:03 +0300

    Exception: Create timeout exception with operation name
    Like withTimeout() exception

---
+++ src/main/kotlin/exception/OperationTimeoutException.kt
@@ -0,0 +1,6 @@
+package exception
+
+class OperationTimeoutException(val timeout: Long, val operationName: String) :
+    QueueElementException("Operation \"$operationName\" timed out ${
+        if(timeout < 0) "immediately" else "in $timeout ms"
+    }")
