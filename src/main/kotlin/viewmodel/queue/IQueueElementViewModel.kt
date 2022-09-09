b2e9ce04fed76818fddd99e2476f20c23d35ba01
darefox <easymalink@gmail.com>
2022-09-09 19:59:32 +0300

    IQueueElement: change interface
    Changes:
    - Make initialize() and save() return deffered exceptions as indicator of failure or succuess of operation
    - If element is busy, initialize() and save() will return null instead of Deferred<Thorwable?>
    - Change signature to initializeAsync and saveAsync()
    - Rename QueueElementStatus enum class to Status
    - Remove setPathToSave() function
    - Rename field pathToSave to savedTo
    - Convert savedTo from String? to  StateFlow<String?>
    - Document all fields, methods, values

--- src/main/kotlin/viewmodel/queue/IQueueElementViewModel.kt
+++ src/main/kotlin/viewmodel/queue/IQueueElementViewModel.kt
@@ -3,23 +3,92 @@ package viewmodel.queue
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.StateFlow
-import ui.ISelectable
 
-sealed interface IQueueElementViewModel : ISelectable {
-    enum class QueueElementStatus {
+sealed interface IQueueElementViewModel {
+    /**
+     * Values:
+     *
+     * [INITIALIZING] - Element is initializing
+     *
+     * [READY_TO_USE] - Element initialized and waits a new operation
+     *
+     * [IN_USE] - Element is downloading something
+     *
+     * [ERROR] - Shows that element operation has failed. Usually set with [an error message][IQueueElementViewModel.lastErrorMessage]
+     *
+     * [SAVED] - Element finished downloading successfully with no errors
+     */
+    enum class Status {
+        /**
+         * Element is initializing
+         */
         INITIALIZING,
-        ERROR,
+
+        /**
+         * Element initialized and waits a new operation
+         */
         READY_TO_USE,
+
+        /**
+         * Element is downloading something
+         */
         IN_USE,
+
+        /**
+         * Shows that element operation has failed. Usually set with [an error message][IQueueElementViewModel.lastErrorMessage]
+         */
+        ERROR,
+
+        /**
+         * Element finished downloading successfully with no errors
+         */
         SAVED
     }
 
-    val status: StateFlow<QueueElementStatus>
+    /**
+     * Current status of element. [Values][Status]
+     */
+    val status: StateFlow<Status>
+
+    /**
+     * Shows last error message of element
+     */
     val lastErrorMessage: StateFlow<String?>
+
+    /**
+     * Shows last progress of queue element
+     */
     val progress: StateFlow<String?>
-    val pathToSave: String?
+
+    /**
+     * Current job of queue element
+     */
     val currentJob: StateFlow<Job?>
-    suspend fun initialize()
-    suspend fun save(): Deferred<Boolean>
-    fun setPathToSave(folder: String)
+
+    /**
+     * Path to folder where element is saved
+     */
+    val savedTo: StateFlow<String?>
+
+    /**
+     * Start initializing operation async.
+     *
+     * If returned value is null, it means that QueueElement is busy with [currentJob] and
+     * your request was ignored
+     *
+     * If queue element isn't busy, you'll get Deferred nullable throwable, showing how operation ended.
+     * If throwable is null, then operation finished successfully
+     */
+    fun initializeAsync(): Deferred<Throwable?>?
+
+    /**
+     * Start saving operation async.
+     *
+     * If returned value is null, it means that QueueElement is busy with [currentJob] and
+     * your request was ignored
+     *
+     * If queue element isn't busy, you'll get Deferred nullable throwable, showing how operation ended.
+     * If throwable is null, then operation finished successfully
+     */
+    fun saveAsync(): Deferred<Throwable?>?
 }
