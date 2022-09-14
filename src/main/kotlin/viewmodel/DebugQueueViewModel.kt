cfa89c199c4ea4d0e0a8f54f6659d7efafb130d6
DareFox <easymalink@gmail.com>
2022-09-14 17:46:31 +0300

    DebugQueue: Set getter instead of const variable
    Because after removing from queue, it will remember result. Getter fixes it by generatign new element on call

--- src/main/kotlin/viewmodel/DebugQueueViewModel.kt
+++ src/main/kotlin/viewmodel/DebugQueueViewModel.kt
@@ -4,7 +4,8 @@ import viewmodel.queue.EntryQueueElementViewModel
 import viewmodel.queue.IQueueElementViewModel
 
 object DebugQueueViewModel {
-    val startQueue = listOf<IQueueElementViewModel>(
-        EntryQueueElementViewModel("https://dtf.ru/apitest/1296731-sohranenie-stati-test-2")
-    )
+    val startQueue
+        get() = listOf<IQueueElementViewModel>(
+            EntryQueueElementViewModel("https://dtf.ru/apitest/1296731-sohranenie-stati-test-2")
+        )
 }
