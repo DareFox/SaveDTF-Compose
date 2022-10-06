83df92ccff74c48d3120f5e6efbbe0644252eec2
DareFox <easymalink@gmail.com>
2022-10-06 15:09:24 +0300

    Shared: Create ISaveable (queue elements) and Abstract class of it
    It's same queue elements (bookmarks, profile and etc.), but in shared package and with other naming

---
+++ shared/src/main/kotlin/shared/saveable/ISaveable.kt
@@ -0,0 +1,97 @@
+package shared.saveable
+
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.StateFlow
+
+sealed interface ISaveable {
+    /**
+     * Values:
+     *
+     * [INITIALIZING] - Element is initializing
+     *
+     * [READY_TO_USE] - Element initialized and waits a new operation
+     *
+     * [IN_USE] - Element is downloading something
+     *
+     * [ERROR] - Shows that element operation has failed. Usually set with [an error message][ISaveable.lastErrorMessage]
+     *
+     * [SAVED] - Element finished downloading successfully with no errors
+     */
+    enum class Status {
+        /**
+         * Element is initializing
+         */
+        INITIALIZING,
+
+        /**
+         * Element initialized and waits a new operation
+         */
+        READY_TO_USE,
+
+        /**
+         * Element is downloading something
+         */
+        IN_USE,
+
+        /**
+         * Shows that element operation has failed. Usually set with [an error message][ISaveable.lastErrorMessage]
+         */
+        ERROR,
+
+        /**
+         * Element finished downloading successfully with no errors
+         */
+        SAVED
+    }
+
+    /**
+     * Current status of element. [Values][Status]
+     */
+    val status: StateFlow<Status>
+
+    /**
+     * Shows last error message of element
+     */
+    val lastErrorMessage: StateFlow<String?>
+
+    /**
+     * Shows last progress of queue element
+     */
+    val progress: StateFlow<String?>
+
+    /**
+     * Current job of queue element
+     */
+    val currentJob: StateFlow<Job?>
+
+    /**
+     * Path to folder where element is saved
+     */
+    val savedTo: StateFlow<String?>
+
+    /**
+     * Start initializing operation async.
+     *
+     * If returned value is null, it means that QueueElement is busy with [currentJob] and
+     * your request was ignored
+     *
+     * If queue element isn't busy, you'll get Deferred nullable throwable, showing how operation ended.
+     * If throwable is null, then operation finished successfully
+     */
+    fun initializeAsync(): Deferred<Throwable?>?
+
+    /**
+     * Start saving operation async.
+     *
+     * If returned value is null, it means that QueueElement is busy with [currentJob] and
+     * your request was ignored
+     *
+     * If queue element isn't busy, you'll get Deferred nullable throwable, showing how operation ended.
+     * If throwable is null, then operation finished successfully
+     */
+    fun saveAsync(): Deferred<Throwable?>?
+
+    override fun hashCode(): Int
+    override fun equals(other: Any?): Boolean
+}
