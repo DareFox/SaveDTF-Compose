6d9de958827c535c58574d53110359322838bf69
DareFox <easymalink@gmail.com>
2022-12-15 00:45:54 +0300

    Shared-DownloadUrl: refactor and change name to getInpusStreamFromUrl

--- shared/src/main/kotlin/shared/ktor/DownloadUrl.kt
+++ shared/src/main/kotlin/shared/ktor/DownloadUrl.kt
@@ -25,7 +25,7 @@ suspend fun HttpClient.downloadUrl(
     timeoutInSeconds: Int,
     directory: File
 ): File? {
-    val inputStream = cache.getValueOrNull(url) ?: download(
+    val inputStream = cache.getValueOrNull(url) ?: getInputSteamFromUrl(
         timeoutInSeconds = timeoutInSeconds,
         url = url,
         retryAmount = retryAmount,
@@ -48,7 +48,7 @@ suspend fun HttpClient.downloadUrl(
     return file
 }
 
-private suspend fun HttpClient.download(
+private suspend fun HttpClient.getInputSteamFromUrl(
     url: String,
     retryAmount: Int,
     replaceOnError: BinaryMedia?,
@@ -56,7 +56,6 @@ private suspend fun HttpClient.download(
 ): InputStream? {
     var attemptCounter = 0
     do {
-        attemptCounter++
         timeout?.join() // Wait if there is too many requests
         val caught = runCatching { // Catch exceptions with runCatching, try-catch don't work with coroutines
             val shouldUseTimeoutRestriction = timeoutInSeconds > 0
@@ -89,32 +88,31 @@ private suspend fun HttpClient.download(
         yield()
         val response = caught.getOrNull()
 
-        // Log error
-        if (response == null) {
-            logger.error { "Response is null" }
-            logger.error { "Caught exception: ${caught.exceptionOrNull()} " }
-        }
+        when (response?.status) {
+            // Log error
+            null -> {
+                logger.error { "Response is null" }
+                logger.error { "Caught exception: ${caught.exceptionOrNull()} " }
+            }
+            // Suspend all requests for 30 seconds on too many requests error
+            HttpStatusCode.TooManyRequests -> {
+                logger.info { "Too many requests. Delaying next requests to 30 seconds" }
+                timeout = scope.launch {
+                    delay(30000L)
+                }
+            }
+            HttpStatusCode.OK -> {
+                val type = response.contentType()
 
-        // Suspend all requests for 30 seconds on too many requests error
-        if (response?.status == HttpStatusCode.TooManyRequests) {
-            logger.info { "Too many requests. Delaying next requests to 30 seconds" }
-            timeout = scope.launch {
-                delay(30000L)
+                if (type == null) {
+                    logger.error { "Content-type of $url response is null. Can't create BinaryMedia metadata" }
+                    return null
+                }
+
+                return response.content.toInputStream()
             }
         }
         yield()
-
-        // On success, return content and break loop
-        if (response?.status == HttpStatusCode.OK) {
-            val type = response.contentType()
-
-            if (type == null) {
-                logger.error { "Content-type of $url response is null. Can't create BinaryMedia metadata" }
-                return null
-            }
-
-            return response.content.toInputStream()
-        }
 
         // If retryAmount is bigger than 0, then try until attemptCounter becomes equal to retryAmount
         // If retryAmount is 0 or less, repeat request infinitely until success
